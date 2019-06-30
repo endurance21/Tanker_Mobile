7c98bdff5d5abf71709663f8e19320ccd01d9101
Aniket Kumar <ani1998ket@gmail.com>
2019-06-30 14:47:36 +0530

    Added new spirtes for buttons

--- js/Screens/GameScreen.js
+++ js/Screens/GameScreen.js
@@ -6,38 +6,62 @@ class GameScreen extends Scene{
         this.gameWorld.addGameObject( new Bird(this.getLayer(2), new Vector2d(50, 427), 30, 30));
         this.gameWorld.addGameObject(new GameObject(this.getLayer(1), new Vector2d(100, 50), 50, 400));
 
-        let btnImageUp = new Image();
-        let btnImageDown = new Image();
-        btnImageUp.src = "assets/images/Glossy01.png";
-        btnImageDown.src = "assets/images/Glossy02.png";
+        let leftButtonImage = new Image();
+        let rightButtonImage = new Image();
+        let upButtonImage = new Image();
+        let downButtonImage = new Image();
+        let leftButtonPressedImage = new Image();
+        let rightButtonPressedImage = new Image();
+        let upButtonPressedImage = new Image();
+        let downButtonPressedImage = new Image();
+        let fireButtonImage = new Image();
+        let fireButtonPressedImage = new Image();
+
+        leftButtonImage.src   = "assets/images/tanker_control_buttons_left.png";
+        rightButtonImage.src  = "assets/images/tanker_control_buttons_right.png";
+        upButtonImage.src     = "assets/images/tanker_control_buttons_up.png";
+        downButtonImage.src   = "assets/images/tanker_control_buttons_down.png";
+        fireButtonImage.src   = "assets/images/tanker_control_buttons_fire.png";
+
+        leftButtonPressedImage.src   = "assets/images/tanker_control_buttons_left_pressed.png";
+        rightButtonPressedImage.src  = "assets/images/tanker_control_buttons_right_pressed.png";
+        upButtonPressedImage.src     = "assets/images/tanker_control_buttons_up_pressed.png";
+        downButtonPressedImage.src   = "assets/images/tanker_control_buttons_down_pressed.png";
+        fireButtonPressedImage.src   = "assets/images/tanker_control_buttons_fire_pressed.png";
 
         let buttonWidth = GAME_WIDTH / 10;
         let uiLayer = this.getLayer(3);
 
         let upButton = new ImageButton(
                           uiLayer,
-                          btnImageUp, btnImageDown,
+                          upButtonImage, upButtonPressedImage,
                           new Vector2d(2 * buttonWidth, 3/4 * GAME_HEIGHT),
                           buttonWidth, buttonWidth);
         let leftButton = new ImageButton (
                           uiLayer,
-                          btnImageUp, btnImageDown,
+                          leftButtonImage, leftButtonPressedImage,
                           new Vector2d(1 * buttonWidth, (3/4 * GAME_HEIGHT) + buttonWidth),
                           buttonWidth, buttonWidth);
         let rightButton = new ImageButton(
                           uiLayer,
-                          btnImageUp, btnImageDown,
+                          rightButtonImage, rightButtonPressedImage,
                           new Vector2d(3 * buttonWidth, (3/4 * GAME_HEIGHT) + buttonWidth),
                           buttonWidth, buttonWidth);
         let downButton = new ImageButton (
                           uiLayer,
-                          btnImageUp, btnImageDown,
+                          downButtonImage, downButtonPressedImage,
                           new Vector2d(2 * buttonWidth, (3/4 * GAME_HEIGHT) + (2*buttonWidth)),
+                          buttonWidth, buttonWidth);
+        let fireButton = new ImageButton (
+                          uiLayer,
+                          fireButtonImage, fireButtonPressedImage,
+                          new Vector2d(GAME_WIDTH - 2 * buttonWidth, (3/4 * GAME_HEIGHT) + (buttonWidth)),
                           buttonWidth, buttonWidth);
         this.gameWorld.addGameObject(upButton);
         this.gameWorld.addGameObject(downButton);
         this.gameWorld.addGameObject(rightButton);
         this.gameWorld.addGameObject(leftButton);
+        this.gameWorld.addGameObject(fireButton);
         let bird = this.gameWorld.getGameObject(0);
         upButton.onClick = ()=>{
           bird.velocity = new Vector2d(0, -5);
