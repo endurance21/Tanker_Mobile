7e69ef4949c924bb2ee3acdbbc1818a728ec2ee8
endurance21 <43696525+endurance21@users.noreply.github.com>
2019-07-04 20:08:19 +0530

    fixed bullet problem

--- js/GameObjects/Tank.js
+++ js/GameObjects/Tank.js
@@ -17,24 +17,10 @@ class Tank extends GameObject{
   }
 
   fire(){
-    let tankVelocity = new Vector2d(0,0);
+    let tankVelocity = new Vector2d(0,-1);
     let angle = this.angle;
-    let position = new Vector2d(this.position.x + this.width/2 , this.position.y + this.height/2);
+    let position = new Vector2d(this.position.x + this.width/2 -this.bullets[0].width/2, this.position.y + this.height/2-this.bullets[0].height/2);
 
-    switch (angle) {
-      case 0:
-              tankVelocity = Up;
-        break;
-      case PI/2:
-              tankVelocity = Right;
-        break;
-      case PI:
-              tankVelocity = Down;
-        break;
-      case 3 * PI/2:
-              tankVelocity = Left;
-        break;
-    }
 
     this.bullets[0].position = position;
     this.bullets[0].anchorPoint = new Vector2d(this.bullets[0].position.x + this.bullets[0].width /2, this.bullets[0].position.y + this.bullets[0].height/2);
--- js/init.js
+++ js/init.js
@@ -5,9 +5,9 @@ const GAME_WIDTH = 480;
 
 const GRAVITY = 0.2;
 const PI = Math.PI;
-const Up = new Vector2d(0,-1);
-const Down = new Vector2d(0,1);
-const Right = new Vector2d(1,0);
-const Left = new Vector2d(-1,0);
+// const Up = new Vector2d(0,-1);
+// const Down = new Vector2d(0,1);
+// const Right = new Vector2d(1,0);
+// const Left = new Vector2d(-1,0);
 
  
